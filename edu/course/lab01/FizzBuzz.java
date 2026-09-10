0b6283cba33378a7328bf702920913534535b64b
pineas <platonsotnicenko390@gmail.com>
2026-09-10 16:52:22 +0700

    feat: Lab1 realization

---
+++ edu/course/lab01/FizzBuzz.java
@@ -0,0 +1,17 @@
+package edu.course.lab01;
+
+public class FizzBuzz {
+    public static void run() {
+        for (int i = 1; i <= 500; i++) {
+            if (i % 5 == 0 && i % 7 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("fizz");
+            } else if (i % 7 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
--- edu/course/lab01/Main.java
+++ edu/course/lab01/Main.java
@@ -1,4 +1,78 @@
 package edu.course.lab01;
 
 public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            printUsage();
+            return;
+        }
+
+        String command = args[0].toLowerCase();
+
+        try {
+            switch (command) {
+                case "fizzbuzz":
+                    if (args.length != 1) {
+                        System.out.println("Error: The 'fizzbuzz' command does not require extra arguments.");
+                        printUsage();
+                        return;
+                    }
+                    FizzBuzz.run();
+                    break;
+
+                case "reverse":
+                    if (args.length != 2) {
+                        System.out.println("Error: The 'reverse' command requires exactly 1 argument (string).");
+                        printUsage();
+                        return;
+                    }
+                    TextTasks.reverse(args[1]);
+                    break;
+
+                case "quadratic":
+                    if (args.length != 4) {
+                        System.out.println("Error: The 'quadratic' command requires exactly 3 arguments (a, b, c).");
+                        printUsage();
+                        return;
+                    }
+                    QuadraticEquation.solve(args[1], args[2], args[3]);
+                    break;
+
+                case "series":
+                    if (args.length != 1) {
+                        System.out.println("Error: The 'series' command does not require extra arguments.");
+                        printUsage();
+                        return;
+                    }
+                    SeriesCalculator.run();
+                    break;
+
+                case "palindrome":
+                    if (args.length != 2) {
+                        System.out.println("Error: The 'palindrome' command requires exactly 1 argument (string).");
+                        printUsage();
+                        return;
+                    }
+                    TextTasks.palindrome(args[1]);
+                    break;
+
+                default:
+                    System.out.println("Error: Unknown command '" + args[0] + "'");
+                    printUsage();
+            }
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
+        }
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage: <command> [arguments]");
+        System.out.println("Available commands:");
+        System.out.println("  fizzbuzz                  - Iterates numbers from 1 to 500 with 5 and 7 multiplicity replacement");
+        System.out.println("  reverse \"<string>\"        - Reverses the string using charAt()");
+        System.out.println("  quadratic <a> <b> <c>     - Solves a quadratic equation");
+        System.out.println("  series                    - Calculates the infinite series sum with 10^-6 precision");
+        System.out.println("  palindrome \"<string>\"     - Checks if the string is a palindrome using two pointers");
+    }
 }
